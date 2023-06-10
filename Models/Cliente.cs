c045d390dacfb5aa4a94cf9049ee284b955565e0
Kissland Baker <Kissland_baker@ucne.edu.do>
2023-06-10 18:23:57 -0400

    Creando el modelo Cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,10 @@
+namespace PizzaPlace.Shared
+{
+  public class Cliente
+  {
+    public int Id { get; set; }     
+    public string Nombre { get; set; } = default!;     
+    public string Calle { get; set; } = default!;     
+    public string Ciudad { get; set; } = default!;
+  }
+}
