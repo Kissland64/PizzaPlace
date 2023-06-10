f88075e7782a3930ac46879e4c03641b8bf365ac
Kissland Baker <Kissland_baker@ucne.edu.do>
2023-06-10 18:21:52 -0400

    Organizando los modelos y creando la BLL

---
+++ BLL/Menu.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic; using System.Linq;
+namespace PizzaPlace.Shared
+{
+  public class Menu
+  {
+    public List<Pizza> Pizzas { get; set; } = new List<Pizza>();
+    public void Agregar(Pizza pizza) => Pizzas.Add(pizza);
+    public Pizza? GetPizza(int id) => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
+  }
+}
