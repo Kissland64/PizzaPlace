c40941f9279f60d9b4557169655ca5a5eb702749
Kissland Baker <Kissland_baker@ucne.edu.do>
2023-06-11 13:08:13 -0400

    Eliminando unos espacios en el modelo CompraCesta y agregando una Extension para depurar al cliente

--- Models/CompraCesta.cs
+++ Models/CompraCesta.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace PizzaPlace.Shared
 {
@@ -7,7 +7,6 @@ namespace PizzaPlace.Shared
     public Cliente Cliente { get; set; } = new Cliente();     
     public List<int> Pedidos { get; set; } = new List<int>();     
     public bool HaPagado { get; set; }
-
     public void Add(int pizzaId) => Pedidos.Add(pizzaId);
     public void RemoveAt(int pos) => Pedidos.RemoveAt(pos);
   }
