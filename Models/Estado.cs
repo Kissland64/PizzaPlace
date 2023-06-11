91f6addb0210a89f908e332d511be7e343384b4b
Kissland Baker <Kissland_baker@ucne.edu.do>
2023-06-11 14:22:17 -0400

    Arreglando el estado y la BLL

--- Models/Estado.cs
+++ Models/Estado.cs
@@ -2,7 +2,7 @@ namespace PizzaPlace.Shared
 {
   public class Estado
   {
-    public Menu Menu { get; } = new Menu();     
+    public MenuBLL Menu { get; } = new MenuBLL();     
     public CompraCesta Cesta { get; } = new CompraCesta();
     public Interfaz Interfaz { get; set; } = new Interfaz();
     public decimal PrecioTotal => Cesta.Pedidos.Sum(id => Menu.GetPizza(id)!.Precio);
